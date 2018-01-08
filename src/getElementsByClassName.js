d87f0367ae64040a28d4c4c4dbb646b6b4f70a5b
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-01-08 12:17:39 -0800

    Implements working getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,57 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // array for storing elements with matching class name
+
+
+var getElementsByClassName = function(className) {
+
+
   var elementsWithClass = [];
-  // array of elements in document.body
-
-  // recurse til bodyElements.length is 0, retrieving classLists
-
-  	//retrieve classList of each element and recurse through
-  	//base case: if classList[x] === className add to hasClass
-  var nodes = document.body.childnodes;
-
-  
 
 
+  (function filterForClassName(node) {
+  		var classList = node.classList;
 
 
+  		if (classList && classList.contains(className)) {
+  			elementsWithClass.push(node);
+  		}
 
+  		if (node.childNodes && node.childNodes.length > 0) {
+  			for (var i = 0; i < node.childNodes.length; i++) {
+  				filterForClassName(node.childNodes[i]);
+  			}
+  		}
+
+
+  }(document.body));
+
+  return elementsWithClass;
 }
 
- if (nodes.length === 0) {
- 	return elementsWithClass;
- } else {
- 	filterObjForClass(nodes[nodes.length - 1], elementsWithClass);
- }
 
-};
-
-// takes obj checks all child nodes and childrens child nodes for matching elements
-// returns obj of all matching nodes
-function filterObjForClass (obj, matchArray) {
-	if (obj.childnodes.length === 0 && hasClass(obj, className)) {
-		matchArray.push(obj);
-	} else {
-		for (var i = obj.childnodes.length; i < obj.childnodes.length; i++) {
-			if (hasClass(obj)) {
-				matchArray.push(obj);
-			}
-			filterObjForClass (obj)
-		}
-	}
-
-
-
-//takes classList and returns true if contains the className
-function hasClass (classList, className) {
-	if (classList.length === 0) {
-		return false;
-	} else if (classList[classList.length - 1] === className) {
-		return true;
-	} else {
-		hasClass(classList.slice(0, classList.length - 1));
-	}
-}
